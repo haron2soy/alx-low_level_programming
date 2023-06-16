f5280b1f45ed9ad58f56c9308277b46f65a1bbff
Haron Soy <haron2soy@gmail.com>
2023-06-16 12:15:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Print 0 to 9.
+ *
+ * Return: Always void
+ */
+int main(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		printf("%d", j);
+	}
+	putchar('\n');
+	return (0);
+}
