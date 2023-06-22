920383a35c688f023d802bd1a24b42cdc2df7b62
Haron Soy <haron2soy@gmail.com>
2023-06-22 12:45:03 -0700

    main.h

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-* main - test for positive or negative
-* return: 0
+* main -test function for a positive or negativ num
+* Return: 0
 */
 int main(void)
 {
-	int i;
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
 }
