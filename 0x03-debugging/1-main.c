8a649dec1feb82fc7ee6a40fdc3601a254260402
Haron Soy <haron2soy@gmail.com>
2023-06-22 13:08:51 -0700

    bettyworks

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 /**
 * main - leads to an infinite loop
-* return: 0
+*
+* Return: Always (0) Success
 */
+
 int main(void)
 {
 	int i;
+
 	printf("Infinite loop incoming :(\n");
 	i = 0;
-	/*while (i < 10)
+	/**
+	* while (i < 10)
 	*{
 	*putchar(i);
-	*}*/
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
