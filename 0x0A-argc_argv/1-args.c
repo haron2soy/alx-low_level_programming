6f725563a4b6428b7bcd646ba81e704eef7504f8
Haron Soy <haron2soy@gmail.com>
2023-07-07 08:53:29 -0700

    secondcommitmesage

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,13 +3,11 @@
 
 /**
  * main - outputs number of arguments passed
- * 
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0
  */
-
 int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
