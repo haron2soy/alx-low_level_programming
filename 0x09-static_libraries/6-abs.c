f120cdfaf7e660a2ec272ec664c285b2499d304f
Haron Soy <haron2soy@gmail.com>
2023-07-07 09:51:05 -0700

    commit message

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - calculator abolute value of an number
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
